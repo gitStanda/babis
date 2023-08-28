80d71e346f43dd60b5a7236e664fa6ce7d8a3c7b
standa <st.urban27@gmail.com>
2023-08-28 02:54:58 +0200

    added show report dialog cuz it wasnt commited first time??

---
+++ lib/components/show_report_dialog.dart
@@ -0,0 +1,28 @@
+import 'package:babisappka/components/open_fb.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+Future<void> showReportDialog(BuildContext context) async {
+  await showDialog(
+    context: context,
+    builder: (_) => CupertinoAlertDialog(
+      title: const Text('Nahlásit chybu'),
+      content: const Text(
+          'Veškeré chyby databáze nahlašuj ve skupině "Sorry Yako" na FB - klidně jako anonym - ta se o data stará.'),
+      actions: [
+        TextButton.icon(
+          onPressed: openFacebook,
+          icon: const Icon(Icons.facebook_rounded),
+          label: const Text("Sorry Yako"),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text(
+            "OK",
+            style: TextStyle(fontWeight: FontWeight.w900),
+          ),
+        ),
+      ],
+    ),
+  );
+}
